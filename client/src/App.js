f0760dfebe427f8508ca41e32d9f985acdc6081d
Sahan Pasindu <mdspsd@gmail.com>
2023-06-07 11:30:39 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [salary, setSalary] = useState(0);
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Name</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age</label>
+        <input type="number" />
+        <label>Country</label>
+        <input type="text" />
+        <label>Position</label>
+        <input type="text" />
+        <label>Salary</label>
+        <input type="number" />
+        <button>Add Employee</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
